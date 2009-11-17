a398d347e247674a9df1f2062fd2cfcac907d3cc
robert.coup <robert.coup@4133c332-d35f-11de-b7d7-53e004a27ad6>
2009-11-17 21:26:28 +0000

    Fix some compile errors under GCC4.3

--- cpp/GPolyEncoder.h
+++ cpp/GPolyEncoder.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <memory>
 
 class GPolyEncoder {
 
--- cpp/gpolyencode_test.cpp
+++ cpp/gpolyencode_test.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <time.h>
+#include <cstdlib>
 
 using namespace std;
 
